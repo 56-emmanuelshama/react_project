2c1d3b228d23ac15c930e7d509e49719a4ff98fd
Emmanuel Shama <shamaemmanuel56@gmail.com>
2026-05-08 14:23:51 +0300

    added photos

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import cybersecuritypic from "./asset/cybersecuritypic.png";
 import shamapic from "./asset/shamapic.png";
 import emmanuelpic from "./asset/emmanuelpic.png";
+import shama from "./asset/shamapic8.jpg"
+import pato from "./asset/shamapic7.jpg"
+import witaba from "./asset/shamapic5.jpg"
+import paul from "./asset/shamapic4.jpg"
+import keziah from "./asset/shamapic3.jpg"
+import ezra from "./asset/shamapic2.jpg"
+import sharon from "./asset/shamapic7.jpg"
+import grace from "./asset/shamapic3.jpg"
+import faith from "./asset/shamapic2.jpg"
+import coro from "./asset/shamapic8.jpg"
+import coro from "./asset/shamapic8.jpg"
 import "./card.css";
 
 function Card() {
@@ -10,7 +21,7 @@ function Card() {
    <section className="cards">
     <div  className="card">
 
-      <img src={cybersecuritypic} alt="Cybersecurity" />
+      <img src={shama} alt="Cybersecurity" />
       <h2>This is my cybersecurity image Card</h2>
         
    </div>
@@ -27,7 +38,7 @@ function Card() {
     <h2>this is shama's pc </h2>
    </div>
    <div className="card">
-     <img src={shamapic} alt="shamapic" />
+     <img src={} alt="shamapic" />
     <h2>this is shama's pc </h2>
    </div>
    <div className="card">
